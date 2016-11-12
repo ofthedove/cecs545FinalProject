1c34c08e113a513e028228f6b87025b04ef7bee6
ofthedove <ajcomb01@louisville.edu>
2016-11-11 23:10:57 -0500

    First attempt at tracking data. Don't like it, going to re-write

---
+++ cecs545FinalProject/cecs545FinalProject/Log.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cecs545FinalProject
+{
+    class Log
+    {
+        const int MAX_RET_STR_LEN = 1000;
+
+        private class Message
+        {
+            public string msg;
+        }
+
+        private List<Message> log;
+
+        public Log()
+        {
+
+        }
+
+        public void Write(string msgIn)
+        {
+            var newMsg = new Message() { msg = msgIn };
+            log.Add(newMsg);
+        }
+
+        public string Read(int start, int end)
+        {
+            if (start + 30 < end)
+            {
+                throw new ArgumentException("May not request more than 30 entries at a time!");
+            }
+
+            string returnString = "";
+            for(int i = start; i < end; i++)
+            {
+                returnString += log[start];
+            }
+            return returnString;
+        }
+    }
+}
